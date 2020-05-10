2c9bb746fbaf401ee929b3f5ebeeb23e6a7353fe
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-05-10 18:56:52 +0200

    Add files via upload

---
+++ 58A.py
@@ -0,0 +1,12 @@
+s = input().lower()
+l = 'hello'
+while True:
+    for x in range(len(s)):
+        for y in range(len(l))
+            if s[x] == l[y]:
+
+                continue
+            else:
+                for z in range(len(l))
+                    l[z]=l[z+1]
+
