aca21b05fa16422a850a8723ca0096f841154feb
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-08-13 14:04:51 +0200

    Create day_of_programmer.py

---
+++ hackerank/day_of_programmer.py
@@ -0,0 +1,28 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def dayOfProgrammer(year):
+    if year == 1918:
+        return "26.09.1918"
+    elif (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0) or year < 1918:
+        result = '12.09.'
+        return result + str(year)
+    else:
+        result = '13.09.'
+        return result + str(year)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
