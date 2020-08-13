a6e42b2d61870361f4f4339c43a0c3cf68bfaf66
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-08-13 14:24:08 +0200

    Create designer_pdf.py

---
+++ hackerank/designer_pdf.py
@@ -0,0 +1,28 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def designerPdfViewer(h, word):
+    alphabet = []
+    for x in word:
+        temp = ord(x) % 97
+        alphabet.append(h[temp])
+    highest = max(alphabet)
+    return highest * len(word)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
