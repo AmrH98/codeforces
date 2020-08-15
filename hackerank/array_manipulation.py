1cfff4ed77468500300c549d53aaa87f1581770d
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-08-15 19:29:30 +0200

    Update array_manipulation.py

--- hackerank/array_manipulation.py
+++ hackerank/array_manipulation.py
@@ -6,14 +6,26 @@ import random
 import re
 import sys
 
-# Complete the arrayManipulation function below.
-def arrayManipulation(n, queries):
-    answer = [0] * n
-    for array in queries:
-        print('Amr')
-        for i in range(((array[0]-1)), array[1]):
-            answer[i]+= array[-1]
-    return max(answer)
+def arrayManipulation(m, n, queries):
+    result = [0] * (n + 1)
+    count = 0
+    highest = 0
+    for x in queries:
+        result[(x[0]) - 1 ] += x[2]
+        result[x[1]] -= x[2]
+    for y in result:
+        count += y
+        if count > highest:
+            highest = count
+    return highest
+
+    # time limit reached
+    #     for i in range(x[0]-1, x[1]):
+    #         result[i] += x[2]
+    # return max(result)
+        
+
+ 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -29,7 +41,7 @@ if __name__ == '__main__':
     for _ in range(m):
         queries.append(list(map(int, input().rstrip().split())))
 
-    result = arrayManipulation(n, queries)
+    result = arrayManipulation(m, n, queries)
 
     fptr.write(str(result) + '\n')
 
