cc875a41dc703a6ae517e02656fb2e6032602145
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-09-05 17:50:42 +0200

    Create cube.cpp

---
+++ hackerearth/cube.cpp
@@ -0,0 +1,25 @@
+
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+using namespace std;
+const unsigned int M = 1000000007;
+int main() {
+    int t, n;
+    cin >> t;                   
+    while(t > 0){
+        cin >> n;
+        int answer, answer2;
+        answer = fmod(pow(n,3),M);
+        answer2 = fmod(pow((n-2),3),M);
+        answer -= answer2;
+        if(n==1){
+            answer = 1;
+        }
+
+        cout << answer << endl;
+        t--;
+    }
+    return 0;
+}
