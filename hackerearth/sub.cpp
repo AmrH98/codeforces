3fb68764c0324a244e30f889c968246d7c61217f
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-09-05 17:20:09 +0200

    Create sub.cpp

---
+++ hackerearth/sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+using namespace std;
+const unsigned int M = 1000000007;
+int main() {
+    // int t, n, k;
+    string s;
+    cin >> s;                              
+    int len = s.size();
+    cout << (len*(len+1)/2);
+    return 0;
+}
