c1226b6ed24b3c62bb06ee437707191925eaef1e
Amr Hassan Hafez <amrhassan98@hotmail.com>
2022-10-22 13:35:21 +0200

    Create 876.cpp

---
+++ leetcode/876.cpp
@@ -0,0 +1,39 @@
+
+//Runtime: 9 ms, faster than 5.51% of C++ online submissions for Middle of the Linked List.
+//Memory Usage: 7.1 MB, less than 21.64% of C++ online submissions for Middle of the Linked List.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *temp; // new node;
+        temp = head;
+        int count = 0; // count number of nodes
+        while(temp != NULL){
+            temp = temp-> next;
+            count++;
+        }
+        int mid = count%2 == 0 ? count/2+1 : count/2+1; // get mid and if 2 mids get higher mid;
+        temp = head; 
+        while(mid > 1){
+            temp = temp->next;
+            mid--;
+
+        }
+        return temp;
+
+
+    }
+    
+};
