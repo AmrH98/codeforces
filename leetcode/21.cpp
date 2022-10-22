534f378f63f8e06723c7e2e744b17e85a9736353
Amr Hassan Hafez <amrhassan98@hotmail.com>
2022-10-22 13:00:14 +0200

    21.cpp explanation

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -18,9 +18,9 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode *answer, *temp; 
         answer = new ListNode(); // new empty node
-        temp = answer; // 
-        while(list1 && list2){
-            if(list1->val < list2->val){
+        temp = answer; // temp pointer to first node
+        while(list1 && list2){ // while lists not empty
+            if(list1->val < list2->val){ // if list1 value smaller than list 2 we increment list1 index by 1 and vice versa
                 temp->next = list1;
                 list1 = list1->next;
             }
@@ -28,10 +28,10 @@ public:
                 temp->next = list2;
                 list2 = list2->next;
             }
-            temp = temp->next;
+            temp = temp->next; // value added to first node of temp, go to next node
         }
         if(list1){
-            temp->next = list1;
+            temp->next = list1; // put rest of list to temp list 
         }
         if(list2){
             temp->next = list2;
