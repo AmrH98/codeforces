c5d5fe04ac727be8c4a223862cf75e3434a4d4c1
Amr Hassan Hafez <amrhassan98@hotmail.com>
2022-10-23 18:44:33 +0200

    Create 121.cpp

---
+++ leetcode/121.cpp
@@ -0,0 +1,24 @@
+//Runtime: 356 ms, faster than 19.33% of C++ online submissions for Best Time to Buy and Sell Stock.
+//Memory Usage: 93.4 MB, less than 9.95% of C++ online submissions for Best Time to Buy and Sell Stock.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int high=prices[0], min=prices[0], profit=0;
+        for(int i=0; i < prices.size(); i++){
+            if(prices[i] < min) {
+                profit = max(profit, high-min);
+                high = prices[i];
+                min = prices[i];
+            }else{
+                high = max(high, prices[i]);
+            }
+        }
+
+        
+        profit = max(profit, high-min);
+        return profit;
+
+
+    }
+};
