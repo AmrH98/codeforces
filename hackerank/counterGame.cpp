6f8b312eeb6900e5b7b03d8b399774c7407dfe48
Amr Hassan Hafez <amrhassan98@hotmail.com>
2022-11-21 14:15:47 +0200

    Create counterGame.cpp

---
+++ hackerank/counterGame.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+string counterGame(long n) {
+    int count = 1;
+    while(n!=1){
+        if(floor(log2(n))==ceil(log2(n))) n/=2;
+        else n-=pow(2,floor(log2(n)));
+        count++;
+    }
+    return (count%2==0) ? "Louis" : "Richard";
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        string result = counterGame(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
