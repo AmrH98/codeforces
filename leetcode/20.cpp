7af02ae70e8b76a2d350ff6aae5dcc55ee1f6984
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-07 15:10:43 +0200

    Update 20.cpp

--- leetcode/20.cpp
+++ leetcode/20.cpp
@@ -1,16 +1,22 @@
 // Runtime 0 ms
-// Memory 6.3 MB
+// Memory 6.4 MB
+
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;  
+        stack<char> string;
         for(auto temp:s){
-            if(temp=='(' or temp=='{' or temp=='[') st.push(temp);  
-            else {
-                if(st.empty() or (st.top()=='(' and temp!=')') or (st.top()=='{' and temp!='}') or (st.top()=='[' and temp!=']')) return false;
-            st.pop(); 
+            if(temp=='(' or temp=='{' or temp=='['){
+                string.push(temp); // add bracket start to stack
+            }
+            else{
+                if(string.empty() || (string.top()=='(' && temp!=')') || (string.top()=='[' && temp!=']') || (string.top()=='{' && temp!='}')){
+                    return false; // if stack empty ( no chars ) or bracket start isn't closed return false
+                }
+                string.pop(); // if bracket is closed, remove bracket open and clear stack
             }
         }
-        return st.empty(); 
+        return string.empty(); // if we reached this level stack is empty so we return true, it means that all brackets are opened and closed
     }
+         
 };
