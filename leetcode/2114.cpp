29a0c7342de9ba518ea1f756e7fac4468c7c86e3
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-09 16:44:52 +0200

    Update 2114.cpp

--- leetcode/2114.cpp
+++ leetcode/2114.cpp
@@ -5,16 +5,15 @@ class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
         string temp;
-        vector<int> ans(sentences.size(),1);
+        vector<int> ans(sentences.size(),1); // initilize vector with 1 ( to count first word)
         for(int i =0; i<sentences.size();i++){
-            // temp = sentences[i];
-            sort(sentences[i].begin(), sentences[i].end());
-            for(int j=0;j<sentences[i].size();j++){
-                if(isspace(sentences[i][j])) ans[i]++;
-                else break;
+            sort(sentences[i].begin(), sentences[i].end()); // sort sentence so white space is first element 
+            for(int j=0;j<sentences[i].size();j++){ // iterate till we hit a char
+                if(isspace(sentences[i][j])) ans[i]++; 
+                else break; // end sentence loop
             }        
         }
-        sort(ans.begin(), ans.end());
+        sort(ans.begin(), ans.end()); // sort and return last element
         return ans.back();
     }
 };
