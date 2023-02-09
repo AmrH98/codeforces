785c5ad07b5c171d361ad946c57c00b78db582da
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-09 20:09:19 +0200

    Create 14.cpp

---
+++ leetcode/14.cpp
@@ -0,0 +1,24 @@
+// Runtime 3 ms Beats 89.47% 
+// Memory 9.2 MB Beats 82.37%
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        char temp;
+        string ans;
+        bool flag =1;
+        if(strs[0].empty()) return "";
+        for(int i=0;i<strs[0].size();i++){
+            temp = strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(temp != strs[j][i]){
+                    flag =0;
+                    break;
+                }
+            }
+            if(flag==0) break;
+            ans+= temp;
+        }
+        return ans;
+    }
+};
