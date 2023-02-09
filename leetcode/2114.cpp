2efb87c857573f2bdeb7c2e23dbf1342bf712543
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-09 16:40:05 +0200

    Update 2114.cpp

--- leetcode/2114.cpp
+++ leetcode/2114.cpp
@@ -1,21 +1,18 @@
-// Runtime26 msBeats13.64%
-//Memory10 MBBeats39.12%
+// Runtime 12 ms Beats 72.39%
+//Memory 9.8 MB Beats 71.40%
+
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
         string temp;
-        int size=sentences.size();
-        vector<int> ans(size);
-        for(int i =0; i<size;i++){
-            temp = sentences[i];
-            int size2=temp.size();
-            int count=1;
-            sort(temp.begin(), temp.end());
-            for(int j=0;j<size2;j++){
-                if(isspace(temp[j])) count++;
+        vector<int> ans(sentences.size(),1);
+        for(int i =0; i<sentences.size();i++){
+            // temp = sentences[i];
+            sort(sentences[i].begin(), sentences[i].end());
+            for(int j=0;j<sentences[i].size();j++){
+                if(isspace(sentences[i][j])) ans[i]++;
                 else break;
             }        
-            ans[i] = count;
         }
         sort(ans.begin(), ans.end());
         return ans.back();
