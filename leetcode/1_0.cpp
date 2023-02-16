d2554bf96ce53da40a578c1d3d478aa866ad972b
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-16 16:00:51 +0200

    Create 1_0.cpp

---
+++ leetcode/1_0.cpp
@@ -0,0 +1,22 @@
+// Runtime 6 ms Beats 97.92%
+// Memory 10.8 MB Beats 45.60%
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> temp;
+        vector<int> ans;
+        int sub;
+        for(int i=0; i<nums.size();i++){
+            if(temp.find(nums[i]) != temp.end()){
+                ans.push_back(temp[nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            sub = target - nums[i];
+            temp[sub] = i;
+        }
+        return ans;
+
+    }
+};
