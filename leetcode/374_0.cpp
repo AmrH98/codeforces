241200136208683cee71a56a65c9b851d1e45d43
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-19 14:23:23 +0200

    Create 374_0.cpp

---
+++ leetcode/374_0.cpp
@@ -0,0 +1,8 @@
+//same runtime and memory as 374.cpp
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
