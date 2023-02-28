66e9757b21cd837ed3dbb09f91a6a225ca7a19a1
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-02-28 15:35:23 +0200

    Update 290.cpp

--- leetcode/290.cpp
+++ leetcode/290.cpp
@@ -1,3 +1,23 @@
+// Runtime 0 ms Beats 100%
+// Memory 6.5 MB Beats 62.14%
+    
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, int> mpchar;
+        unordered_map<string, int> mpword;
+        istringstream stream(s);
+        string word;
+        int i=0, size = pattern.size();
+        for(word; stream>>word;i++){ // stream chars till hit space, word found
+            if(i==size || mpchar[pattern[i]] != mpword[word]) return false; // if words exceeded patern size return false or if pattern char does not match word
+            mpchar[pattern[i]]=i+1; // increment both maps to same value i+1 as unique
+            mpword[word]=i+1;
+        }
+        return i==size;
+    }
+};
+
 //Runtime 4 ms Beats 37.51%
 // Memory 6.4 MB Beats 62.14%
 
