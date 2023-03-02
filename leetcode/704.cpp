1bb225cb94cacae4c969f7a36d6d680b2d0e91ac
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-02 15:47:25 +0200

    Update 704.cpp

--- leetcode/704.cpp
+++ leetcode/704.cpp
@@ -1,4 +1,25 @@
-// Binary Search Iterative Approach
+//Runtime 39 ms Beats 71.22%
+// Memory 27.6 MB Beats 18.49%
+
+class Solution {
+private: 
+    int binarySearch(vector<int>& nums, int low, int high, int target){
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]<target) low = mid+1;
+            else if(nums[mid]>target) high = mid-1;
+            else return mid;
+        }
+        return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0, high = nums.size()-1;
+        return binarySearch(nums, low, high, target);
+    }
+};
+
+// Binary Search Approach
 // Runtime 41 ms Beats 61.19%
 //Memory 27.5 MB Beats 69.7%
 
