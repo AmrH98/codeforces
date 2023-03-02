cb7202321b3ae9e5e3674b157109a7fc159d55fd
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-02 16:58:56 +0200

    Update 74.cpp

--- leetcode/74.cpp
+++ leetcode/74.cpp
@@ -1,7 +1,41 @@
+// BS rows and columns
+//Runtime 3 ms Beats 86.75%
+//Memory 9.6 MB Beats 7.71%
+    
+class Solution {
+private:
+    bool bSearch(vector<int> nums, int target, int high, int low){
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]<target) low=mid+1;
+            else if(nums[mid] > target) high = mid-1;
+            else return true;
+        }
+        return false;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int high = matrix.size()-1, low =0;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(matrix[mid][0]==target) return true;
+            else if(matrix[mid][0] < target && matrix[mid+1][0]>target){
+                low = mid;
+                break;
+            }
+            else if(matrix[mid][0] < target) low = mid+1;
+            else high = mid-1;
+        }
+        int lowCol=0;
+        high=matrix[low].size()-1;
+        return bSearch(matrix[low],target,high,lowCol);
+    }
+};
+
 // Runtime 5 ms Beats 56.20%
 // Memory 9.7 MB Beats 7.71%
 
-class Solution {
+class Solution { // here we did binary search only for columns within a row but not for rows too
 private:
     bool bSearch(vector<int> nums, int target, int high, int low){
         while(low<=high){
