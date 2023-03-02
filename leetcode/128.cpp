671692d1ce8b8889168c1e9532cce067dff53ac5
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-02 18:01:04 +0200

    Update 128.cpp

--- leetcode/128.cpp
+++ leetcode/128.cpp
@@ -18,3 +18,24 @@ public:
         return maxim;
     }
 };
+
+//Runtime 120 ms Beats 82.26%
+// Memory 45.7 MB Beats 77.50%
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> temp(nums.begin(), nums.end()); // set to remove duplicates
+        int size = nums.size();
+        if(size==1) return 1;
+        if(size==0) return 0;
+        int maxim=0;
+        for(auto &num:temp){ // iterate through set elements
+            if(!temp.count(num-1)){ // if first element of the sequence execute 
+                int length=1;
+                while(temp.count(num+length))length++; // iterate length while n+length is in set ...length value ex:[1,2,3,4,5,6]
+                maxim = max(maxim,length); // compare biggest seq with current counted one
+            }
+        }
+        return maxim; 
+    }
+};
