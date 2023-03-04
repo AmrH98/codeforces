9185f5ba2800a5f8f605f6bfbde4bd4e8995a883
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-04 11:24:13 +0200

    Update 169.cpp

--- leetcode/169.cpp
+++ leetcode/169.cpp
@@ -1,3 +1,14 @@
+//Runtime 24 ms Beats 47.73%
+// Memory 19.6 MB Beats 79.79%
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
+
 // runtime 52 ms
 // memory 19.6 MB
 class Solution {
