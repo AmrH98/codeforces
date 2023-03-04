2e96f9610c590c265b71fcd965e5db4c15e598c2
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-04 11:46:35 +0200

    Update 283.cpp

--- leetcode/283.cpp
+++ leetcode/283.cpp
@@ -1,18 +1,26 @@
+// Runtime 19 ms Beats 92.54%
+// Memory 19.1 MB Beats 98.94%
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int move=0;
+        // count moved elements and place non zero element in relative position of variable move
+        for(int i=0;i<nums.size();i++) if(nums[i]!=0) nums[move++] = nums[i];
+        // complete rest of the array with zeros as we already moved all valid elements
+        for(move; move<nums.size();move++) nums[move]=0;
+    }
+};
+
 // Runtime 15 ms Beats 98.70%
 // Memory 19.3 MB Beats 17.16%
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int x=0,y=0;
+        int x=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                y++;
-            }
-            else{
-                swap(nums[x], nums[i]);
-                x++;
-                y++;
+            if(nums[i]!=0){
+                swap(nums[x++], nums[i]);
             }
         }
     }
