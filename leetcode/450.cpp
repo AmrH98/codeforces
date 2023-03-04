1d0938c3bd525a4aee627c175c29caad2c40e55e
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-04 14:43:21 +0200

    Create 450.cpp

---
+++ leetcode/450.cpp
@@ -0,0 +1,44 @@
+// Runtime 29 ms Beats 93.98%
+// Memory 32.7 MB Beats 84.95%
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* minNode(TreeNode* root){
+        TreeNode* curr = root;
+        while(curr && curr->left){ 
+            curr = curr->left;
+        }
+        return curr;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        // 2 cases, either node has 2 descendants, or node has 1 or none
+        if(!root) return root;
+        // find key 
+        if(key < root->val) root->left = deleteNode(root->left, key);
+        else if(key > root->val) root->right = deleteNode(root->right, key);
+        // if found
+        else{
+            if(!root->left) return root->right;
+            else if(!root->right) return root->left;
+            else{
+                TreeNode* minimumNode = minNode(root->right);
+                root->val = minimumNode->val;
+                root->right = deleteNode(root->right, minimumNode->val);
+            }
+        }
+        return root;
+
+    }
+};
