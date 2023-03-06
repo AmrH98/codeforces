482c6b4f44e80bb0f68b8b03f60b1a69077daeeb
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-06 14:25:21 +0200

    Update 724.cpp

--- leetcode/724.cpp
+++ leetcode/724.cpp
@@ -1,7 +1,23 @@
+// Runtime 20 ms Beats 90.76%
+// Memory 31 MB Beats 99.89%
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0, leftSum=0, rightSum=0, size = nums.size();
+        for(int num:nums) sum+=num;
+        for(int i=0;i<size;i++){
+            rightSum = sum - nums[i] - leftSum;
+            if(rightSum == leftSum) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
+
+
+
 // Runtime: 67 ms, faster than 17.12% of C++ online submissions for Find Pivot Index.
 // Memory Usage: 31.2 MB, less than 51.60% of C++ online submissions for Find Pivot Index.
-
-
 
 class Solution {
 public:
