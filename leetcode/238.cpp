bbbfc72ce4d64b73c0a9b56681ae48d9ad0bd3a0
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-13 19:52:11 +0200

    Update 238.cpp

--- leetcode/238.cpp
+++ leetcode/238.cpp
@@ -1,22 +1,23 @@
+
+
 //Runtime 23 ms Beats 85.76%
 //Memory 24 MB Beats 75.19%
 
-//O(n} approach based on leetcode submission 
+//O()N approach based on leetcode submission, space O(1) -> on leetcode submission
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int size = nums.size(), pre=1;
-        vector<int> answer(size);
+        int size = nums.size(), pre=1,suff=1;
+        vector<int> ans(size); // O(n)
         for(int i=0;i<size;i++){
-           answer[i] = pre;
-           pre *= nums[i];
+            ans[i]=pre;
+            pre*= nums[i];
         }
-        pre=1; // post
-        for(int i=size-1;i>=0;i--){
-            answer[i] *= pre;
-            pre*=nums[i];
-        }
-        return answer;
+        for(int i=size-1;i>=0;i--){ // O(n)
+            ans[i] *= suff;
+            suff*=nums[i];
+        } // O(n) +  O(n) = O(2n) =  O(n)
+        return ans;
     }
 };
 
