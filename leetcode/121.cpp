258ba7a09fea4d1d1269686b6cb65a04bad57bd1
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-15 17:16:19 +0200

    Update 121.cpp

--- leetcode/121.cpp
+++ leetcode/121.cpp
@@ -1,3 +1,23 @@
+// Runtime 143 ms Beats 44.38%
+// Memory 93.3 MB Beats 91.62%
+
+// O(n), O(1) space
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0], sell = prices[0], profit=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+                sell = prices[i];
+            }
+            else sell = prices[i];
+            profit = max(profit, sell-buy);
+        }
+        return profit;
+    }
+};
+
 //Runtime: 356 ms, faster than 19.33% of C++ online submissions for Best Time to Buy and Sell Stock.
 //Memory Usage: 93.4 MB, less than 9.95% of C++ online submissions for Best Time to Buy and Sell Stock.
 
