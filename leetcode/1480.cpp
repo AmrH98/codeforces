a1cf135f3420b585a0ae54be313accb875e7b36b
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-15 20:12:32 +0200

    Update 1480.cpp

--- leetcode/1480.cpp
+++ leetcode/1480.cpp
@@ -1,6 +1,20 @@
+// Runtime 0 ms Beats 100% 
+ //Memory 8.5 MB Beats 78.59%
+// O(n) , space O(1)
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++) nums[i]+=nums[i-1];
+        return nums;
+        
+    }
+};
+
+
 // Runtime: 7 ms, faster than 45.70% of C++ online submissions for Running Sum of 1d Array.
 // Memory Usage: 8.5 MB, less than 74.55% of C++ online submissions for Running Sum of 1d Array.
 
+// O(n) , space O(n)
 
 class Solution {
 public:
