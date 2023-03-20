bcdaf7e1a40e829564addfe9c6f5fa98526b56f3
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-20 16:58:25 +0200

    Update 1.cpp

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -1,3 +1,20 @@
+// Runtime 12 ms Beats 81.59%
+// Memory 10.9 MB Beats 27.73%
+
+// O(n), space O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int i=0;i<nums.size();i++){
+            if(map.find(nums[i]) != map.end()) return {map[nums[i]], i};
+            map[target - nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
 //Runtime: 21 ms, faster than 58.77% of C++ online submissions for Two Sum.
 //Memory Usage: 10.8 MB, less than 53.47% of C++ online submissions for Two Sum.
 // TC O(n)
