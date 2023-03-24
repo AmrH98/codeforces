1fc69541f2517b3825d21163704d07bfd903e51b
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-24 19:21:01 +0200

    Update 102.cpp

--- leetcode/102.cpp
+++ leetcode/102.cpp
@@ -1,3 +1,36 @@
+// Runtime 4 ms Beats 65.94%
+// Memory 12.5 MB Beats 86.98%
+
+// O(n) , space O(max(# of nodes in each level)) 
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        queue<TreeNode*> qu;
+        qu.push(root);
+        ans.push_back({root->val});
+        while(!qu.empty()){
+            vector<int> temp;   
+            int curr = qu.size();
+            while(curr-- > 0 ){
+                root = qu.front();
+                qu.pop();
+                if(root->left){
+                    qu.push(root->left);
+                    temp.push_back(root->left->val);
+                }
+                if(root->right){
+                    qu.push(root->right);
+                    temp.push_back(root->right->val);
+                }
+            }
+            if(!temp.empty())ans.push_back(temp);
+        }
+        return ans;        
+    }
+};
+
 // Runtime 7 ms Beats 37.55%
 // Memory 12.6 MB Beats 66.32%
 
