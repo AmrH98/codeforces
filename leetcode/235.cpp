9ba85a51133aa92db89ad8e8ba7a23020fc712c2
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-24 18:00:31 +0200

    Update 235.cpp

--- leetcode/235.cpp
+++ leetcode/235.cpp
@@ -1,16 +1,28 @@
+// iterative
+// O(h), space O(1)
+
+// Runtime 39 ms Beats 20.86%
+// Memory 23.2 MB Beats 76.65%
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return nullptr;
+        while(root){
+            if(root->val < p->val && root->val < q->val) root= root->right;
+            else if(root->val > p->val && root->val > q->val) root =root->left;
+            else return root;
+        }
+        return nullptr;
+    }
+};
+
+
 // Runtime 35 ms Beats 48.99%
 // Memory 23.4 MB Beats 27.85%
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// Recursive
 // root = [6,2,8,0,4,7,9,null,null,3,5]
+// O(h), O(h)
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
