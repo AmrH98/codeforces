e719f9f1ba657f3051dd5e7278f8e8aa99693d38
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-03-24 16:50:20 +0200

    Update 94.cpp

--- leetcode/94.cpp
+++ leetcode/94.cpp
@@ -1,19 +1,56 @@
+
+
+// Runtime 6 ms Beats 20.54%
+// Memory 8.3 MB Beats 78.56%
+
+// O(n) , O(n)
+// iterative approach 
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(!root) return {};
+        vector<int> ans;
+        stack<TreeNode*> stack;
+        while(root || !stack.empty()){
+            while(root){
+                stack.push(root);
+                root= root->left;
+            }
+            root = stack.top();
+            stack.pop();
+            ans.push_back(root->val);
+            root = root->right;
+        }
+        return ans;
+    }
+};
+
+
+// Runtime 0 ms Beats 100%
+// Memory 8.4 MB Beats 45.74%
+// O(n) , O(n)
+class Solution {
+private:
+    void dfs(TreeNode* root, vector<int>& ans){
+        if(!root) return;
+        dfs(root->left, ans);
+        ans.push_back(root->val);
+        dfs(root->right, ans);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        dfs(root, ans);
+        return ans;
+    }
+};
+
 // Runtime 4 ms Beats 28.34%
 // Memory 10.5 MB Beats 7.64%
 
 // O(n), Memory O(n)
 // recursive approach
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 private:
     vector<int> inorder(TreeNode* root, vector<int>& ans) {
