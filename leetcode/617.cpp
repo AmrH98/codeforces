4cdf47dc7b5d32e51d9c61410965df80dca79c4b
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-04-01 23:43:54 +0200

    Update 617.cpp

--- leetcode/617.cpp
+++ leetcode/617.cpp
@@ -1,3 +1,21 @@
+// Runtime 39 ms Beats 70.77%
+// Memory 32.2 MB Beats 90.60%
+
+// better space complexity, not creating new nodes
+// O(max(n,m)), space O(1)?
+// difficult to manage memory safely
+class Solution {
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+            // return build(root1,root2);
+            if(!root1) return root2;
+            if(!root2) return root1;
+            root1->val += root2->val;
+            root1->left = mergeTrees(root1->left, root2->left);
+            root1->right = mergeTrees(root1->right, root2->right);
+            return root1;
+        }
+};
+
 // Runtime 38 ms Beats 75.12%
 // Memory 32.3 MB Beats 66.13%
 
