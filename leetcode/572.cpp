bdcf62e813fa4760c58efefdd8d0e298e59764fb
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-04-01 20:35:29 +0200

    Update 572.cpp

--- leetcode/572.cpp
+++ leetcode/572.cpp
@@ -1,3 +1,24 @@
+// Runtime 27 ms Beats 56.81% 
+// Memory 28.8 MB Beats 61.90
+
+
+class Solution {
+private:
+    bool check(TreeNode* curr, TreeNode*sub){
+        if(!curr && !sub) return true;
+        if(!curr || !sub) return false;
+        if(curr->val != sub->val) return false;
+        return check(curr->left, sub->left) && check(curr->right, sub->right);
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!subRoot) return true;
+        if(!root) return false;
+        if(check(root,subRoot)) return true;
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
+
 // Runtime 24 ms Beats 72.60%
 //Memory 29.1 MB Beats 12.33%
 
