4eb5994fdaee8db9b050cd8fe7dd3d6b13ca8378
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-04-01 06:03:49 +0200

    Update 205.cpp

--- leetcode/205.cpp
+++ leetcode/205.cpp
@@ -1,5 +1,23 @@
-Runtime: 29 ms, faster than 9.02% of C++ online submissions for Isomorphic Strings.
-Memory Usage: 7 MB, less than 45.99% of C++ online submissions for Isomorphic Strings.
+// Runtime 14 ms Beats 18.40% 
+// Memory 7 MB Beats 47.14%
+
+// O(n), space O(n)
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> map, map2;
+        for(int i=0;i<s.length();i++){ // both same size
+            if((map.find(s[i])!=map.end() && map[s[i]] != t[i]) || 
+            ( map2.find(t[i])!=map2.end() && map2[t[i]] != s[i])) return false;
+            map[s[i]] = t[i];
+            map2[t[i]] = s[i];
+        } 
+        return true;
+    }
+};
+
+// Runtime: 29 ms, faster than 9.02% of C++ online submissions for Isomorphic Strings.
+// Memory Usage: 7 MB, less than 45.99% of C++ online submissions for Isomorphic Strings.
 
 class Solution {
 public:
