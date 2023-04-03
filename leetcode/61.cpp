c83456a979d3e98e2185b2b2cb91879bc8e7cdb0
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-04-03 17:37:04 +0200

    Update 61.cpp

--- leetcode/61.cpp
+++ leetcode/61.cpp
@@ -1,3 +1,39 @@
+// Runtime 9 ms Beats 47.52%
+// Memory 11.8 MB Beats 8.27%
+
+// O(n), space O(1)
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k == 0) return head;
+
+        ListNode* curr = head, *newHead, *newTail;
+        int size = 1; 
+        // 1 because we know initially there is 1 node and we want to stop loop at last node
+
+        while(curr->next){
+            size++;
+            curr = curr->next;
+        }
+
+        k %= size;
+        if(k == 0) return head;
+
+        newTail = head;
+
+        // find new tail k + from start, // we use size - k - 1 to get kth node from the end of list  
+        for(int i=0;i<size-k-1;i++) newTail = newTail->next;
+
+        newHead = newTail->next; // head is one step after tail from k
+        newTail->next = nullptr; // so tail disconect it from rest of nodes to nullptr
+        curr->next = head; // connect original head to the new tail
+ // 1,2,3,4,5 -> original head is 1, new head is 4, connect 5 to head which is 1, and 3 is null
+
+        return newHead;
+    }
+};
+
+
 // Runtime 5 ms Beats 79.55%
 // Memory 11.7 MB Beats 61.77%
 
