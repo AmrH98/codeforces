1cec05ee8bc5dc0db0db1f3fd5229cc3c3092fc0
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-04-05 19:37:12 +0200

    Create 1379.cpp

---
+++ leetcode/1379.cpp
@@ -0,0 +1,14 @@
+// Runtime 569 ms Beats 28.76%
+// Memory 163.8 MB Beats 84.51% 
+
+// O(1), O(h)
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(!original) return nullptr; 
+        if(original == target) return cloned; // if found return cloned 
+        TreeNode* left = getTargetCopy(original->left, cloned->left, target); // check all left
+        if(left) return left; // if we stopped at left return it
+        else return getTargetCopy(original->right, cloned->right, target); // else it is in right
+    }
+};
