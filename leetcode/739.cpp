fb267390fe0a117ab2f12896bb5f4b940ff1b344
Amr Hassan Hafez <amrhassan98@hotmail.com>
2023-07-01 21:10:25 +0300

    Update 739.cpp

--- leetcode/739.cpp
+++ leetcode/739.cpp
@@ -1,6 +1,24 @@
-// using stack ...
- // code.......
-
+// using stack
+// Runtime 236 ms Beats 42.31%
+// Memory 107.5 MB Beats 20.22%
+// O(n), O(n) space
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int, int>> st;
+        int size = temperatures.size();
+        vector<int> ans(size);
+        for(int i = 0; i < size; i++){
+            while(!st.empty() && temperatures[i] > st.top().first){
+                int tempIndex = st.top().second;
+                ans[tempIndex] = i - tempIndex;
+                st.pop();
+            }
+            st.push({temperatures[i], i});
+        }
+        return ans;
+    }
+};
 
 
 //code 2
